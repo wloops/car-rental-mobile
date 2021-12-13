99afa2bc977ef8e5ca128692389e586533a4a966
devtint <devtint@gmail.com>
2021-12-13 17:51:41 +0800

    home

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,50 +1,64 @@
 <template>
   <div class="home">
-    <div class="navTop">
-      <van-nav-bar title="惠租车" left-arrow>
-        <template #left>
-          <span>德保县</span>
-        </template>
-        <template #right>
-          <span>注册/登录</span>
-        </template>
-      </van-nav-bar>
-    </div>
-    <div class="switchingRentalMode">
-      <van-tabs v-model="active" animated>
-        <van-tab v-for="index in 4" :title="'选项 ' + index">
-          内容 {{ index }}
-        </van-tab>
-      </van-tabs>
+    <div class="homeTopAll">
+      <div class="navTop">
+        <van-nav-bar title="惠租车" left-arrow>
+          <template #left>
+            <span>德保县</span>
+          </template>
+          <template #right>
+            <span>注册/登录</span>
+          </template>
+        </van-nav-bar>
+      </div>
+      <div class="switchingRentalMode">
+        <van-tabs v-model="tabsActive" animated>
+          <van-tab title="单位租">
+            <!-- <rental-select-time></rental-select-time> -->
+            <van-cell title="选择时间" is-link @click="show = !show" />
+            <van-action-sheet v-model="show" title="标题">
+              <div class="content">内容</div>
+            </van-action-sheet>
+          </van-tab>
+          <van-tab title="个人租">
+            <!-- <rental-select-time></rental-select-time> -->
+          </van-tab>
+        </van-tabs>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { rentalSelectTime } from '@/views/home/components/rentalSelectTime.vue'
+
 import { NavBar } from 'vant'
 import { Tab, Tabs } from 'vant'
+import { ActionSheet } from 'vant'
 
 export default {
   name: 'Home',
   components: {
+    rentalSelectTime,
+
     [NavBar.name]: NavBar,
     [Tab.name]: Tab,
     [Tabs.name]: Tabs,
+    [ActionSheet.name]: ActionSheet,
   },
-  methods: {
-    beforeChange(index) {
-      // 返回 false 表示阻止此次切换
-      if (index === 1) {
-        return false
-      }
-
-      // 返回 Promise 来执行异步逻辑
-      return new Promise(resolve => {
-        // 在 resolve 函数中返回 true 或 false
-        resolve(index !== 3)
-      })
-    },
+  data() {
+    return {
+      tabsActive: 0,
+      show: false,
+    }
   },
+  methods: {},
 }
 </script>
+
+<style long="less">
+.content {
+  padding: 16px 16px 160px;
+}
+</style>
