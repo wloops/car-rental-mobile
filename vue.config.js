3b42c220b765c99f159e69f65d1b7e165836e25f
devtint <devtint@gmail.com>
2021-12-22 14:17:30 +0800

    修复 npm run serve 出现network不出现（公司里没啥用）

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   configureWebpack: {
+//     // other webpack options to merge in ...
+//   },
+
+//   // devServer Options don't belong into `configureWebpack`
+
+//   devServer: {
+//     public: '192.1.2.110:8080',
+
+//     // hot: true,
+
+//     // disableHostCheck: true,
+//   },
+// }
