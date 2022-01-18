bec0f38cf99c16c305f542815518cb3b7be37f05
devtint <devtint@gmail.com>
2022-01-18 11:40:21 +0800

    refactor(vant): vant按需引用改为全局

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { globalRegister } from './global'
+
+// import { Loading, Overlay } from 'vant'
+Vue.use(globalRegister)
+// Vue.use(Loading)
+// Vue.use(Overlay)
 
 Vue.config.productionTip = false
 
