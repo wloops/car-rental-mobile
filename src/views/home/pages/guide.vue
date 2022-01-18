a72664768f4593a28e0bf08a045be4e2ed8186f0
devtint <devtint@gmail.com>
2022-01-18 16:38:14 +0800

    feat(guide): 租车引导页面雏形

--- src/views/home/pages/guide.vue
+++ src/views/home/pages/guide.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="Guide">
     <van-nav-bar title="租车指引" left-arrow @click-left="onClickLeft" />
+    <div class="tabs">
+      <van-tabs v-model="active" scrollspy sticky>
+        <van-tab title="所需证件">
+          <template>
+            <div>
+              <h3>租车需要准备哪些证件?</h3>
+              <van-card centered thumb="https://img01.yzcdn.cn/vant/ipad.jpeg">
+                <template #title>
+                  <div>
+                    <p class="title">身份证</p>
+                  </div>
+                </template>
+                <template #desc>
+                  <div class="desc">
+                    大陆用户驾驶员二代身份证，年满18周岁，有效期需超过当次租期一个月以上
+                  </div>
+                </template>
+              </van-card>
+            </div>
+          </template>
+        </van-tab>
+        <van-tab title="如何预定">
+          <template>
+            <div>
+              <h3>只需四步即可完成预定</h3>
+            </div>
+          </template>
+        </van-tab>
+      </van-tabs>
+    </div>
   </div>
 </template>
 
@@ -10,7 +40,9 @@ export default {
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      active: 0,
+    }
   },
   computed: {},
   watch: {},
@@ -24,4 +56,20 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+h3 {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  text-align: center;
+}
+.desc {
+  font-size: 14px;
+  color: #666;
+  margin-top: 10px;
+}
+.title {
+  font-size: 16px;
+  font-weight: bold;
+}
+</style>
